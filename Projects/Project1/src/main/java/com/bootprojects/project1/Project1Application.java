d74a1a654a8b0daa25f6c177946a595c09bc4101
AK <vermaakshat45@gmail.com>
2025-12-14 22:40:51 +0530

    Testing commit from IntelliJ

--- Projects/Project1/src/main/java/com/bootprojects/project1/Project1Application.java
+++ Projects/Project1/src/main/java/com/bootprojects/project1/Project1Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Project1Application {
 
 	public static void main(String[] args) {
+		System.out.println("HI!");
 		SpringApplication.run(Project1Application.class, args);
 	}
 
